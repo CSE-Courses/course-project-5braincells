cbcbfb7a58bf3f78ca631be49eabc72b91f6baeb
Terence <tli46@buffalo.edu>
2020-10-16 18:47:32 -0400

    Added how to pull the service listing from the database to the instance

--- cse442_App/lib/service_list.dart
+++ cse442_App/lib/service_list.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'userListings_model.dart';
 
 class ServiceList extends StatefulWidget {
   @override
@@ -6,6 +10,44 @@ class ServiceList extends StatefulWidget {
     // TODO: implement createState
     return ServiceListState();
   }
+}
+
+String test = "";
+Future<List<UserListingsModel>> getListing() async {
+    print("Getting listings");
+    
+    final String apiUrl = "https://job-5cells.herokuapp.com/allListings";
+    final response = await http.get(apiUrl);
+
+      final String temp = response.body;
+      return userListingsModelFromJson(temp);
+}
+
+Widget testText() {
+  return Container(
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: () async {
+          final List<UserListingsModel> testingUserList = await getListing();
+          print(testingUserList[0].jobType);
+        },
+        padding: EdgeInsets.all(20.0),
+        shape: RoundedRectangleBorder(
+            side: BorderSide(color: Colors.blue)
+        ),
+        color: Colors.white,
+        child: Text(
+          'refresh',
+          style: TextStyle(
+            color: Colors.blue,
+            letterSpacing: 1.5,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'OpenSans',
+          ),
+        ),
+      )
+  );
 }
 
 class ServiceListState extends State<ServiceList> {
@@ -16,11 +58,25 @@ class ServiceListState extends State<ServiceList> {
         title: Text("List of Services"),
         centerTitle: true,
       ),
-      body: ListView(
-        children: [
-          Text("Test of Service"),
-        ],
-      ),
+      body:Stack(
+        children: <Widget> [
+          Container(
+            height: double.infinity,
+            child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(
+              horizontal: 40.0,
+              vertical: 80.0,
+            ),
+            child: Column(
+              children: <Widget>[
+              SizedBox(height: 30.0),
+              testText(),
+              ]
+            ),
+          ),
+         )
+        ]
+      )
     );
   }
 }
