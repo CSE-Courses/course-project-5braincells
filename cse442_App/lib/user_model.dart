59322725a652549321ed2db6f7507d10246b9b91
yingwei li <yingweili@yingweiteki-MacBook-Pro.local>
2020-10-15 20:28:58 -0400

    Added ratings into the user_mode in flutter

--- cse442_App/lib/user_model.dart
+++ cse442_App/lib/user_model.dart
@@ -1,7 +1,3 @@
-// To parse this JSON data, do
-//
-//     final userModel = userModelFromJson(jsonString);
-
 import 'dart:convert';
 
 UserModel userModelFromJson(String str) => UserModel.fromJson(json.decode(str));
@@ -9,56 +5,61 @@ UserModel userModelFromJson(String str) => UserModel.fromJson(json.decode(str));
 String userModelToJson(UserModel data) => json.encode(data.toJson());
 
 class UserModel {
-    UserModel({
-        this.firstname,
-        this.lastname,
-        this.email,
-        this.password,
-        this.listOfJobs,
-        this.location,
-        this.phone,
-        this.id,
-        this.updatedAt,
-        this.createdAt,
-        this.v,
-    });
+  UserModel({
+    this.firstname,
+    this.lastname,
+    this.email,
+    this.password,
+    this.listOfJobs,
+    this.location,
+    this.phone,
+    this.ratings,
+    this.id,
+    this.updatedAt,
+    this.createdAt,
+    this.v,
+  });
 
-    String firstname;
-    String lastname;
-    String email;
-    String password;
-    List<dynamic> listOfJobs;
-    String location;
-    String phone;
-    String id;
-    DateTime updatedAt;
-    DateTime createdAt;
-    int v;
+  String firstname;
+  String lastname;
+  String email;
+  String password;
+  List<String> listOfJobs;
+  String location;
+  String phone;
+  List<String> ratings;
+  String id;
+  DateTime updatedAt;
+  DateTime createdAt;
+  int v;
 
-    factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
         firstname: json["firstname"],
         lastname: json["lastname"],
         email: json["email"],
         password: json["password"],
-        listOfJobs: List<dynamic>.from(json["listOfJobs"].map((x) => x)),
+        listOfJobs: List<String>.from(json["listOfJobs"].map((x) => x)),
         location: json["location"],
         phone: json["phone"],
+        ratings: List<String>.from(json["ratings"].map((x) => x)),
         id: json["_id"],
         updatedAt: DateTime.parse(json["updatedAt"]),
         createdAt: DateTime.parse(json["createdAt"]),
         v: json["__v"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "firstname": firstname,
+        "lastname": lastname,
         "email": email,
         "password": password,
         "listOfJobs": List<dynamic>.from(listOfJobs.map((x) => x)),
         "location": location,
         "phone": phone,
+        "ratings": List<dynamic>.from(ratings.map((x) => x)),
         "_id": id,
         "updatedAt": updatedAt.toIso8601String(),
         "createdAt": createdAt.toIso8601String(),
         "__v": v,
-    };
+      };
 }
