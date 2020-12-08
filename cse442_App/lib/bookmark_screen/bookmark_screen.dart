97c115d1d345fb78747cca64d0f4d336d7a030cc
yingwei li <yingweili@yingweiteki-MacBook-Pro.local>
2020-12-08 03:07:53 -0500

    task 115, create delete icon and functionality to delete the icon

--- cse442_App/lib/bookmark_screen/bookmark_screen.dart
+++ cse442_App/lib/bookmark_screen/bookmark_screen.dart
@@ -4,9 +4,9 @@ import 'package:http/http.dart' as http;
 import '../user model/user_listings_model.dart';
 import '../user model/user_model.dart';
 import '../user model/singleListing_model.dart';
+import 'dart:convert';
 
 class BookmarkScreen extends StatefulWidget {
-  @override
   final UserModel user;
   BookmarkScreen({this.user});
   State<StatefulWidget> createState() {
@@ -15,13 +15,13 @@ class BookmarkScreen extends StatefulWidget {
 }
 
 class BookmarkScreenState extends State<BookmarkScreen> {
-  final UserModel user;
+  UserModel user;
   BookmarkScreenState({this.user});
 
   List<String> bookmarkList = [];
   List<String> tempList = [];
   List<String> finalList = [];
-  List<SingleListing> finaltemplist;
+  List<SingleListing> finaltemplist = [];
 
   void initState() {
     super.initState();
@@ -35,7 +35,7 @@ class BookmarkScreenState extends State<BookmarkScreen> {
 
     final String apiUrl = "https://job-5cells.herokuapp.com/getById/" + userId;
     final response = await http.get(apiUrl);
-    print(response.body);
+
     if (response.statusCode == 200) {
       final String resString = response.body;
       return userModelFromJson(resString);
@@ -53,12 +53,12 @@ class BookmarkScreenState extends State<BookmarkScreen> {
     final List<SingleListing> randomtemporaryholder =
         singleListingFromJson(finalList.toString());
     setState(() {
+      bookmarkList = newUser.bookmarks;
       finaltemplist = randomtemporaryholder;
-      print(finaltemplist.length);
     });
   }
 
-  void getBookmark(String bookmarkID) async {
+  Future getBookmark(String bookmarkID) async {
     final String apiUrl =
         "https://job-5cells.herokuapp.com/listidFromBook/" + bookmarkID;
     final response = await http.get(apiUrl);
@@ -66,7 +66,7 @@ class BookmarkScreenState extends State<BookmarkScreen> {
     await getListingByBookmark(temp);
   }
 
-  void getListingByBookmark(String listingID) async {
+  Future getListingByBookmark(String listingID) async {
     final String apiUrl =
         "https://job-5cells.herokuapp.com/listingFromBooks/" + listingID;
     final response = await http.get(apiUrl);
@@ -77,20 +77,38 @@ class BookmarkScreenState extends State<BookmarkScreen> {
     });
   }
 
-  Widget getInformationBox(
-    String jobType,
-    String description,
-    String owner,
-  ) {
+  Future deleteBook(String userid, String bid, String lid, int index) async {
+    final String apiUrl = "https://job-5cells.herokuapp.com/bookmarks/delete";
+    final response = await http.post(apiUrl,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: json.encode({
+          "user_id": userid,
+          "bookmark_id": bid,
+        }));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        finaltemplist.removeAt(index);
+      });
+    }
+  }
+
+  Widget getInformationBox(String jobType, String description, String owner,
+      String bId, String lid, int index) {
     return ListTile(
       title: Text(jobType,
           style: TextStyle(
               color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20)),
       subtitle: Text(description, style: TextStyle(color: Colors.white)),
       // tileColor: Colors.blue,
-      leading: Icon(
-        Icons.description,
+      leading: IconButton(
+        icon: Icon(Icons.delete),
         color: Colors.white,
+        onPressed: () async {
+          await deleteBook(user.id, bId, lid, index);
+        },
       ),
       trailing: Icon(
         Icons.keyboard_arrow_right,
@@ -118,27 +136,46 @@ class BookmarkScreenState extends State<BookmarkScreen> {
   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Flex(
-        direction: Axis.vertical,
-        children: [
-          Center(
-            child: Text(
-              "Bookmarked Listings",
-              style: TextStyle(
-                  fontSize: 30.0,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.w600,
-                  fontFamily: "Roboto"),
-            ),
-          ),
-          //this should be in listview or something, gives a error if there is too much listings to the bottom of the screen
-          if (finaltemplist != null)
-            for (var listing in finaltemplist)
-              getInformationBox(
-                  listing.jobType, listing.description, listing.owner),
-        ],
-      ),
+    // return Scaffold(
+    //   body: Flex(
+    //     direction: Axis.vertical,
+    //     children: [
+    //       Center(
+    //         child: Text(
+    //           "Bookmarked Listings",
+    //           style: TextStyle(
+    //               fontSize: 30.0,
+    //               color: Colors.blue,
+    //               fontWeight: FontWeight.w600,
+    //               fontFamily: "Roboto"),
+    //         ),
+    //       ),
+    //       //this should be in listview or something, gives a error if there is too much listings to the bottom of the screen
+
+    //       if (finaltemplist != null)
+    //         for (int i = 0; i < finaltemplist.length; i++)
+    //           // for (var listing in finaltemplist)
+    //           getInformationBox(
+    //               finaltemplist[i].jobType,
+    //               finaltemplist[i].description,
+    //               finaltemplist[i].owner,
+    //               bookmarkList[i]),
+    //     ],
+    //   ),
+    // );
+    return new Scaffold(
+      body: Container(
+          child: ListView.builder(
+              itemCount: finaltemplist.length,
+              itemBuilder: (BuildContext context, int index) {
+                return getInformationBox(
+                    finaltemplist[index].jobType,
+                    finaltemplist[index].description,
+                    finaltemplist[index].owner,
+                    bookmarkList[index],
+                    finaltemplist[index].id,
+                    index);
+              })),
     );
   }
 }
